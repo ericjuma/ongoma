5c54c2915cf6e1769a3547efd8fee7b72b0f3066
phedwin <phedwinsching@students.uonbi.ac.ke>
2025-04-20 14:24:10 +0300

    - Format code - build on ubuntu
    Signed-off-by: phedwin <phedwinsching@students.uonbi.ac.ke>

--- src/Source/MainComponent.h
+++ src/Source/MainComponent.h
@@ -2,22 +2,21 @@
 
 #include <JuceHeader.h>
 
-class MainComponent  : public juce::Component
-{
-public:
-    MainComponent();
-    ~MainComponent() override;
+class MainComponent : public juce::Component {
+       public:
+	MainComponent();
+	~MainComponent() override;
 
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	void paint(juce::Graphics &) override;
+	void resized() override;
 
-private:
-    juce::TextButton beepButton;
+       private:
+	juce::TextButton beepButton;
 
-    // New audio components for making sound
-    juce::AudioDeviceManager deviceManager;
-    juce::AudioSourcePlayer audioSourcePlayer;
-    juce::ToneGeneratorAudioSource toneSource;
+	// New audio components for making sound
+	juce::AudioDeviceManager deviceManager;
+	juce::AudioSourcePlayer audioSourcePlayer;
+	juce::ToneGeneratorAudioSource toneSource;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
